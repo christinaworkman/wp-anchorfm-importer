bc8501242c7b774fe82b81041d8ce357c306708b
Christina Workman <christina.workman@webdevstudios.com>
2022-01-28 10:40:31 -0700

    create main file

---
+++ anchor-fm-importer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Plugin Name: Anchor.fm Importer
+* Plugin URI: https://christinaworkman.ca
+* Description: Plugin to import podcast episodes from Anchor.fm.
+* Version: 0.1
+* Author: Christina Workman
+* Author URI: https://christinaworkman.ca
+* License: GPLv2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: anchor-fm-importer
+**/
