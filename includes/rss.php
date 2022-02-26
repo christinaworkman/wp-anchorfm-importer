c75f2c35e00cf501c5637500bddcc4fb65449e48
Christina Workman <christinakworkman@gmail.com>
2022-02-26 14:30:44 -0700

    replace player variable with placeholder

--- includes/rss.php
+++ includes/rss.php
@@ -12,7 +12,7 @@ $maxitems = 0;
 if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
  
     // Figure out how many total items there are, but limit it to 5. 
-    $maxitems = $rss->get_item_quantity( 5 ); 
+    $maxitems = $rss->get_item_quantity( 2 ); 
  
     // Build an array of all the items, starting with element 0 (first element).
     $rss_items = $rss->get_items( 0, $maxitems );
@@ -25,12 +25,24 @@ endif;
         <li><?php _e( 'No items', 'anchor-fm-importer' ); ?></li>
     <?php else : ?>
         <?php // Loop through each feed item and display each item as a hyperlink. ?>
-        <?php foreach ( $rss_items as $item ) : ?>
-            <li>
-                <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-                    title="<?php printf( __( 'Posted %s', 'anchor-fm-importer' ), $item->get_date('j F Y | g:i a') ); ?>">
-                    <?php echo esc_html( $item->get_title() ); ?>
-                </a>
+        <?php foreach ( $rss_items as $item ) :
+            // Variables for RSS elements 
+            $permalink = $item->get_permalink();
+            $publishDate = $item->get_date('j F Y | g:i a');
+            $episodeId = $item->get_date('YmdHis');
+            $title = $item->get_title();
+            $description = $item->get_description();
+            $player = "audiofile here";
+            ?>
+            
+            <li style="margin-left: 300px">
+                <h2><a href="<?php echo esc_url( $permalink ); ?>"
+                    title="<?php printf( __( 'Posted %s', 'anchor-fm-importer' ), $publishDate ); ?>" 
+                    episodeID="<?php echo $episodeId?>">
+                    <?php echo esc_html( $title ); ?>
+                </a></h2>
+                <p><?php echo html_entity_decode( $description ); ?></p>
+                <?php echo $player ?>
             </li>
         <?php endforeach; ?>
     <?php endif; ?>
